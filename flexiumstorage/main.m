99b74e52454b8d01806a0ddf7fa3f1d51bb08248
flexium <flexium@uk00-b2nb01-2.local>
2017-04-28 08:21:34 +0800

    Initial Commit

---
+++ flexiumstorage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  flexiumstorage
+//
+//  Created by flexium on 2017/4/28.
+//  Copyright © 2017年 FLEXium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
